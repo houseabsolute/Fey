0a5943c5e320014bfbb261feb9b8a28b4290cbff
autarch <devnull@localhost>
2007-01-24 16:49:22 +0000

    Add docs for Fey::SQL::Where.

--- lib/Fey/SQL/Where.pm
+++ lib/Fey/SQL/Where.pm
@@ -11,3 +11,54 @@ use Class::Trait ( 'Fey::Trait::SQL::HasWhereClause' );
 1;
 
 __END__
+
+=head1 NAME
+
+Fey::SQL::Where - Represents a "stand-alone" WHERE clause
+
+=head1 SYNOPSIS
+
+  my $sql = Fey::SQL->new( dbh => $dbh );
+
+  # WHERE Machine.machine_id = 2
+  $sql->where( $machine_id, '=', 2 );
+
+=head1 DESCRIPTION
+
+This class represents a stand-alone C<WHERE> clause. This allows you
+pass a condition as part of an outer join.
+
+=head1 METHODS
+
+This class provides the following methods:
+
+=head2 Constructor
+
+To construct an object of this class, call C<< $query->where() >> on
+a C<Fey::SQL> object.
+
+=head2 $where->where()
+
+See the L<Fey::SQL section on WHERE Clauses|Fey::SQL/WHERE Clauses>
+for more details.
+
+=head1 TRAITS
+
+This class does C<Fey::Trait::SQL::HasWhereClause> trait.
+
+=head1 AUTHOR
+
+Dave Rolsky, <autarch@urth.org>
+
+=head1 BUGS
+
+See C<Fey::Core> for details on how to report bugs.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006-2007 Dave Rolsky, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
