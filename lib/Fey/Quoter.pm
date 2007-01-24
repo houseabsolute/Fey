cc0144174dc02e129499ae8693e5d976dcf2b791
autarch <devnull@localhost>
2007-01-24 00:12:49 +0000

    Docs for Fey::Quoter.

--- lib/Fey/Quoter.pm
+++ lib/Fey/Quoter.pm
@@ -61,3 +61,75 @@ sub join_table_and_column
 1;
 
 __END__
+
+=head1 NAME
+
+Fey::Quoter - Provides quoting and other related string manipulations
+
+=head1 SYNOPSIS
+
+  my $quoter = Fey::Quoter->new( dbh => $dbh )
+
+  print $quoter->quote_identifier( $column->name() );
+  print $quoter->unquote_identifier( $quoted_name );
+  print $quoter->join_table_and_column( $table->name(), $column->name() );
+
+=head1 DESCRIPTION
+
+This class provides methods for quoting and related string
+manipulations needed to generate valid SQL.
+
+=head1 METHODS
+
+This class provides the following methods:
+
+=head2 Fey::Quoter->new( dbh => $dbh )
+
+This method creates a new C<Fey::Quoter> object. It requires a C<DBI>
+handle for the "dbh" parameter.
+
+=head2 $quoter->dbh()
+
+Returns the C<DBI> handle passed to the constructor.
+
+=head2 $quoter->quote_identifier($identifier)
+
+This does the proper quoting for an identifier, like a column or table
+name.
+
+It tries to determines how to do this by asking the C<DBI> handle
+passed to the constructor, using the C<< $dbh->get_info() >>
+method. It falls back to using double-quotes (C<">).
+
+=head2 $quoter->unquote_identifier($identifier)
+
+This strips quotes from an identifier name. It is provided for the
+benefit of the C<Fey::Loader> modules.
+
+=head2 $quoter->quote_string($string)
+
+This quotes a string for use as a value (like in an C<UPDATE> or
+C<INSERT> clause). Internally it just calls C<< $dbh->quote() >>.
+
+=head2 $quoter->join_table_and_column( $table_name, $column_name )
+
+This joins together a table and column to form a fully-qualified
+reference to a column. This always uses the period character (C<.>) as
+the separator.
+
+=head1 AUTHOR
+
+Dave Rolsky, <autarch@urth.org>
+
+=head1 BUGS
+
+See C<Fey::Core> for details on how to report bugs.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006-2007 Dave Rolsky, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
