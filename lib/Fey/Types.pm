36544e24bd13ecae6fcd405bf59d06d103127a2b
Dave Rolsky <autarch@urth.org>
2009-02-04 11:49:16 -0600

    Add docs

--- lib/Fey/Types.pm
+++ lib/Fey/Types.pm
@@ -191,3 +191,32 @@ subtype 'Fey.Type.WhereClauseSide'
 no Moose::Util::TypeConstraints;
 
 1;
+
+__END__
+
+=head1 NAME
+
+Fey::Types - Types for use in Fey
+
+=head1 DESCRIPTION
+
+This module defines a whole bunch of types used by the Fey core
+classes. None of these types are documented for external use at the
+present, though that could change in the future.
+
+=head1 AUTHOR
+
+Dave Rolsky, <autarch@urth.org>
+
+=head1 BUGS
+
+See L<Fey> for details on how to report bugs.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2006-2009 Dave Rolsky, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
